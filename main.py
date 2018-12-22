d451cd8c7a7a7aaa9c2655cf942f21bb32fae796
arkadiuszsas <kontakt.arkadiusz.sas@gmail.com>
2018-12-22 19:16:21 +0100

    fixed display functions

--- main.py
+++ main.py
@@ -93,10 +93,10 @@ def getInfo(player_ID, URL):
     height = re.sub(player_ID + r'/[\w\s\.\']+" [\w\=\_\"\.]+>[\w\s\']+</a>\r\n\s*[\<>/\w]+\r\n\s*\r\n\s*<div>\r\n\s*[\<>=_";:&?\/\-\,\w\s]+title="[\w\s\-]+"[\w\s\-\"<>:;=\/\.]+\r\n\s*[\w\s\=<>"?&_\/\-\,]+title="[\w]+[\w\s\-\="><:;\/\.]+\r\n\s*[\w\s\=_"?&><:;\/\.\,\-]+\r\n\s*</span>\r\n\s*</div>\r\n\s*</div>\r\n\s*</td>\r\n\s*[\w\s\<>="]+">\d{2}[\w\<>/]+\r\n\s*[\w\s\<="]+">\w{2,3}</td>\r\n\s*[\w\s\<="->]+</td>\r\n\s*[\w\s\<>="_-]+">[\d\.]+\w{1}[\w\s\<="_>/\.\-]+\r\n\s*<td>\d{1}[\w\s\<>=:;/"\-]+\r\n\s*<td>\d{1}[\w\s\=":;<>/\-\\]+>[H,L,M]{1}</span></td>\r\n\s*<td><[\w\s\="\-]+>\d{2}</span></td>\r\n\s*<td><[\w\s\=_"\-]+>\d{2}</span></td>\r\n\s*<td><[\w\s\=_"\-]+>\d{2}</span></td>\r\n\s*<td><[\w\s\=_"\-]+>\d{2}</span></td>\r\n\s*<td><[\w\s\=_"\-]+>\d{2}</span></td>\r\n\s*<td><[\w\s\=_"\-]+>\d{2}</span></td>\r\n\s*<td>', "", info_full[0])
     
     #print(info_full)
-    print("ID: {0}\nName: {1}\nClub: {2}\nNation: {3}\nHeight: {4} cm\nOverall: {5}\nPosition: {6}\nPrice: {7}\n\nSTATS:\nPace: {8}\nShooting: {9}\nPassing: {10}\nDribbling: {11}\nDefending: {12}\nPhysicality: {13}\n".format(id, name, club, nation, height, overall, position, price, pace, shooting, passing, dribbling, defending, physicality))
+    #print("ID: {0}\nName: {1}\nClub: {2}\nNation: {3}\nHeight: {4} cm\nOverall: {5}\nPosition: {6}\nPrice: {7}\n\nSTATS:\nPace: {8}\nShooting: {9}\nPassing: {10}\nDribbling: {11}\nDefending: {12}\nPhysicality: {13}\n".format(id, name, club, nation, height, overall, position, price, pace, shooting, passing, dribbling, defending, physicality))
 
     # return part
-    dane = [id, name, club, nation, height, overall, pace, shooting, passing, dribbling, defending, physicality]
+    dane = [id, name, club, nation, position, height, overall, price, pace, shooting, passing, dribbling, defending, physicality]
     return dane
 
 # DATABASE PART
@@ -165,6 +165,45 @@ def readData(position, id_list):
     for player in players:
         id_list.append(player['id'])
 
+# create table which contains player's squad
+cur.execute("DROP TABLE IF EXISTS squad;")
+cur.execute("""
+ CREATE TABLE IF NOT EXISTS squad (
+     id varchar(250) UNIQUE,
+     squad_position varchar(250) NOT NULL UNIQUE
+ );""")
+
+# squad players list
+squad = (('239', 'gk'), ('434', 'rb'), ('489', 'lb'),
+         ('421', 'cb_1'), ('427', 'cb_2'), ('291', 'cm_1'),
+         ('592', 'cm_2'), ('720', 'lm'), ('308', 'rm'), ('418', 'st_1'), ('143', 'st_2'))
+
+# execute many records
+cur.executemany('INSERT INTO squad VALUES(?,?)', squad)
+
+# auxiliary function that short information about player's squad
+def displaySquad():
+    cur.execute('SELECT squad.id, squad.squad_position FROM squad')
+    players = cur.fetchall()
+    for player in players:
+        if (player['squad_position'] == 'gk'):
+            url = URL_GK
+        elif (player['squad_position'] == 'rb'):
+            url = URL_RB
+        elif (player['squad_position'] == 'lb'):
+            url = URL_LB
+        elif (player['squad_position'] == 'cb_1' or player['squad_position'] == 'cb_2'):
+            url = URL_CB
+        elif (player['squad_position'] == 'cm_1' or player['squad_position'] == 'cm_2'):
+            url = URL_CM
+        elif (player['squad_position'] == 'lm'):
+            url = URL_LM
+        elif (player['squad_position'] == 'rm'):
+            url = URL_RM
+        elif (player['squad_position'] == 'st_1' or player['squad_position'] == 'st_2'):
+            url = URL_ST
+        full_info = getInfo(player['id'], url)
+        print(full_info[4], ": ", full_info[1])
 
 # USER PART
 
@@ -172,43 +211,59 @@ def readData(position, id_list):
 def displayGK():
     readData('gk', id_list)
     for player in id_list:
-        getInfo(player, URL_GK)
+        full_info = getInfo(player, URL_GK)
+        print("ID: {0}\nName: {1}\nClub: {2}\nNation: {3}\nHeight: {4} cm\nOverall: {5}\nPosition: {6}\nPrice: {7}\n\nSTATS:\nPace: {8}\nShooting: {9}\nPassing: {10}\nDribbling: {11}\nDefending: {12}\nPhysicality: {13}\n".format(full_info[0], full_info[1], full_info[2], full_info[3], full_info[4], full_info[5], full_info[6], full_info[7], full_info[8], full_info[9], full_info[10], full_info[11], full_info[12], full_info[13]))
 def displayRB():
     readData('rb', id_list)
     for player in id_list:
-        getInfo(player, URL_RB)
+        full_info = getInfo(player, URL_RB)
+        print("ID: {0}\nName: {1}\nClub: {2}\nNation: {3}\nHeight: {4} cm\nOverall: {5}\nPosition: {6}\nPrice: {7}\n\nSTATS:\nPace: {8}\nShooting: {9}\nPassing: {10}\nDribbling: {11}\nDefending: {12}\nPhysicality: {13}\n".format(full_info[0], full_info[1], full_info[2], full_info[3], full_info[4], full_info[5], full_info[6], full_info[7], full_info[8], full_info[9], full_info[10], full_info[11], full_info[12], full_info[13]))
 def displayLB():
     readData('lb', id_list)
     for player in id_list:
-        getInfo(player, URL_LB)
+        full_info = getInfo(player, URL_LB)
+        print("ID: {0}\nName: {1}\nClub: {2}\nNation: {3}\nHeight: {4} cm\nOverall: {5}\nPosition: {6}\nPrice: {7}\n\nSTATS:\nPace: {8}\nShooting: {9}\nPassing: {10}\nDribbling: {11}\nDefending: {12}\nPhysicality: {13}\n".format(full_info[0], full_info[1], full_info[2], full_info[3], full_info[4], full_info[5], full_info[6], full_info[7], full_info[8], full_info[9], full_info[10], full_info[11], full_info[12], full_info[13]))
 def displayCB():
     readData('cb', id_list)
     for player in id_list:
-        getInfo(player, URL_CB)
+        full_info = getInfo(player, URL_CB)
+        print("ID: {0}\nName: {1}\nClub: {2}\nNation: {3}\nHeight: {4} cm\nOverall: {5}\nPosition: {6}\nPrice: {7}\n\nSTATS:\nPace: {8}\nShooting: {9}\nPassing: {10}\nDribbling: {11}\nDefending: {12}\nPhysicality: {13}\n".format(full_info[0], full_info[1], full_info[2], full_info[3], full_info[4], full_info[5], full_info[6], full_info[7], full_info[8], full_info[9], full_info[10], full_info[11], full_info[12], full_info[13]))
 def displayCM():
     readData('cm', id_list)
     for player in id_list:
-        getInfo(player, URL_CM)
+        full_info = getInfo(player, URL_CM)
+        print("ID: {0}\nName: {1}\nClub: {2}\nNation: {3}\nHeight: {4} cm\nOverall: {5}\nPosition: {6}\nPrice: {7}\n\nSTATS:\nPace: {8}\nShooting: {9}\nPassing: {10}\nDribbling: {11}\nDefending: {12}\nPhysicality: {13}\n".format(full_info[0], full_info[1], full_info[2], full_info[3], full_info[4], full_info[5], full_info[6], full_info[7], full_info[8], full_info[9], full_info[10], full_info[11], full_info[12], full_info[13]))
 def displayLM():
     readData('lm', id_list)
     for player in id_list:
-        getInfo(player, URL_LM)
+        full_info = getInfo(player, URL_LM)
+        print("ID: {0}\nName: {1}\nClub: {2}\nNation: {3}\nHeight: {4} cm\nOverall: {5}\nPosition: {6}\nPrice: {7}\n\nSTATS:\nPace: {8}\nShooting: {9}\nPassing: {10}\nDribbling: {11}\nDefending: {12}\nPhysicality: {13}\n".format(full_info[0], full_info[1], full_info[2], full_info[3], full_info[4], full_info[5], full_info[6], full_info[7], full_info[8], full_info[9], full_info[10], full_info[11], full_info[12], full_info[13]))
 def displayRM():
     readData('rm', id_list)
     for player in id_list:
-        getInfo(player, URL_RM)
+        full_info = getInfo(player, URL_RM)
+        print("ID: {0}\nName: {1}\nClub: {2}\nNation: {3}\nHeight: {4} cm\nOverall: {5}\nPosition: {6}\nPrice: {7}\n\nSTATS:\nPace: {8}\nShooting: {9}\nPassing: {10}\nDribbling: {11}\nDefending: {12}\nPhysicality: {13}\n".format(full_info[0], full_info[1], full_info[2], full_info[3], full_info[4], full_info[5], full_info[6], full_info[7], full_info[8], full_info[9], full_info[10], full_info[11], full_info[12], full_info[13]))
 def displayST():
     readData('st', id_list)
     for player in id_list:
-        getInfo(player, URL_ST)
+        full_info = getInfo(player, URL_ST)
+        print("ID: {0}\nName: {1}\nClub: {2}\nNation: {3}\nHeight: {4} cm\nOverall: {5}\nPosition: {6}\nPrice: {7}\n\nSTATS:\nPace: {8}\nShooting: {9}\nPassing: {10}\nDribbling: {11}\nDefending: {12}\nPhysicality: {13}\n".format(full_info[0], full_info[1], full_info[2], full_info[3], full_info[4], full_info[5], full_info[6], full_info[7], full_info[8], full_info[9], full_info[10], full_info[11], full_info[12], full_info[13]))
 
+
+# main program
+        
 id_list = []
 
 while(True):
     option = input("\nWhat do you want to do? \n\t1. Show my squad\n\t2. Modify my squad\n\t3. Display players \nOption: ")
 
-    if (option == '3'):
+    if (option == '1'):
+        displaySquad()
+        continue
+
+    if (option == '4'):
         player_position = input("Which position do you want to display? \n\t1. GK\n\t2. RB\n\t3. LB\n\t4. CB\n\t5. CM\n\t6. LM\n\t7. RM\n\t8. ST\nPosition: ")
+        print("\n")
         if (player_position == 'GK' or player_position == 'gk' or player_position == '1'):
             displayGK()
         elif (player_position == 'RB' or player_position == 'rb' or player_position == '2'):
